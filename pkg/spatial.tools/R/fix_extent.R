c5dbe77d4a0b4c1e43384eb2ebd5fd2283a7c8b6
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-18 21:03:42 +0000

    Added new function: fix_extent
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatial-tools@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/spatial.tools/R/fix_extent.R
@@ -0,0 +1,28 @@
+#' Forces a list of Raster*s to all have the same extent.
+#' @title fix_extent
+#' @param extent_reference Raster*. A Raster* object that will provide the extent to all the other Raster*s.  If unassigned, will assume it is the first Raster* in the broken_extents list. 
+#' @param broken_extents list of Raster* objects. Raster* objects that will be coerced to the extent_reference's extent.
+#' @author Jonathan A. Greenberg (\email{spatial.tools@@estarcion.net})
+#' @seealso \code{\link{extent}} \code{\link{stack}}
+#' @examples
+#' # TODO 
+#' @export
+
+
+fix_extent <- function(extent_reference,broken_extents)
+{
+	if(missing(extent_reference))
+	{
+		extent_reference = broken_extents[[1]]
+	}
+	
+	base_extent=extent(extent_reference)
+	fixed_extents=mapply(
+		function(x,base_extent) 
+		{ 
+			extent(x)=base_extent
+			return(x)
+		},broken_extents,MoreArgs=list(base_extent=base_extent) 
+	)
+	return(fixed_extents)
+}
