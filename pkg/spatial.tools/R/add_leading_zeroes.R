f928306ae61948b710269eb1dc7e4b63d3e44626
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-18 21:14:04 +0000

    Added new function:add_leading_zeroes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatial-tools@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/spatial.tools/R/add_leading_zeroes.R
@@ -0,0 +1,37 @@
+#' add_leading_zeroes
+#' Add Leading Zeroes to a Numeric Vector
+#' 
+#' Appends leading zeroes to a vector of numbers based on a string length or a
+#' maximum number.
+#' 
+#' 
+#' @param number A numeric vector.
+#' @param number_length The length of the output string.
+#' @param max_number A number to base the length of the output string on.
+#' @return A character vector.
+#' @author Jonathan A. Greenberg
+#' @seealso \code{\link[climstats]{brickstack_to_saved_rasters}}
+#' @keywords format
+#' @examples
+#' 
+#' x=c(1:10)
+#' add_leading_zeroes(x,number_length=4)
+#' add_leading_zeroes(x,max_number=10000)
+#' 
+#' @export
+
+add_leading_zeroes=function(number,number_length,max_number)
+{
+	if(missing(max_number))
+	{
+		max_number=max(number)	
+	}
+	if(missing(number_length))
+	{
+		number_length=floor(log10(max_number))+1
+	}
+	
+	
+	fmt=paste("%0",number_length,"d",sep="")
+	return(sprintf(fmt,number))
+}
