f695edbee2125f4789d25d04ddaf575d5b0b1cf3
jgrn307 <jgrn307@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-25 18:51:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spatial-tools@130 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/spatial.tools-alpha/building_spatial_tools.R
+++ pkg/spatial.tools-alpha/building_spatial_tools.R
@@ -1,6 +1,6 @@
 # For tauboa:
 package_dir <- "/Users/jgrn307/Documents/workspace/spatial-tools/pkg/spatial.tools"
-package_dir <- "D:\\Users\\jgrn\\Documents\\code\\workspace\\spatial-tools\\pkg\\spatial.tools"
+package_dir <- "D:\\Users\\jgrn\\workspace\\spatial-tools\\pkg\\spatial.tools"
 
 setwd(package_dir)
 
